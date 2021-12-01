e0fb2716087aad6c162768580d6e9d096f77f553
Devesh Tyagi <tyagi77@outlook.com>
2021-12-01 12:04:32 +0530

    Initial Commit

---
+++ LeadEssentials/LeadEssentials.h
@@ -0,0 +1,18 @@
+//
+//  LeadEssentials.h
+//  LeadEssentials
+//
+//  Created by Devesh Tyagi on 01/12/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LeadEssentials.
+FOUNDATION_EXPORT double LeadEssentialsVersionNumber;
+
+//! Project version string for LeadEssentials.
+FOUNDATION_EXPORT const unsigned char LeadEssentialsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LeadEssentials/PublicHeader.h>
+
+
